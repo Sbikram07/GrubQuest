8eaaaf015fbd4db9002648807e204d98e681e652
Bikram <bikramsasmal2707@gmail.com>
2025-08-16 02:01:04 +0530

    adjusting cookies options

--- server/src/utility/cookieOptions.js
+++ server/src/utility/cookieOptions.js
@@ -2,7 +2,7 @@ const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production", // true in production (HTTPS)
   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-  sameSite: "Lax", // or "Strict" based on your frontend/backend config
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // or "Strict" based on your frontend/backend config
 };
 
 module.exports = cookieOptions;
